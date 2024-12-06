911ffb0f9eb71fafd3be3dd209df1e0efc56bd73
foxy <foxy@foxy.com>
2024-12-07 02:21:06 +0700

    build app

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ module.exports = {
   async rewrites() {
     return [
       {
+        source: '/api/:path*',
+        destination: '/api/:path*', // Handle API routes natively
+      },
+      {
         source: '/:path*',
         destination: '/index.html', // This serves React app for non-API routes
       },
@@ -35,15 +39,15 @@ module.exports = {
     ];
   },
   reactStrictMode: true,
-  // Optional custom configuration (e.g., redirects or headers)
-  // async headers() {
-  //   return [
-  //     {
-  //       source: '/api/:path*',
-  //       headers: [
-  //         { key: 'Cache-Control', value: 'no-store' }
-  //       ],
-  //     },
-  //   ];
-  // },
+  // Optional custom configuration(e.g., redirects or headers)
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Cache-Control', value: 'no-store' }
+        ],
+      },
+    ];
+  },
 };
